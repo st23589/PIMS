24ed335c25b358a95f137c7a18859a5fbeb33a89
st23589 <st23589@howick.school.nz>
2026-08-04 10:48:55 +1200

    Split the Function into Name Verifying, Licence verifying and warrent check planning on adding fine calculating and finishing record_offence

--- PIMS.py
+++ PIMS.py
@@ -1,17 +1,17 @@
 recorded_offenses = {}
+outstanding = ['Connor Riley','Rhys Henwood','Max Homan','Niek Erkkila','Jack Lilly']
 def menu():
     print('''
     Police Patrol System
        1. Record a speeding offense
        2. View all recorded offenses
-       3. Search offence records
+       3. Search offense records
        4. Display patrol summary
        5. Exit Program
     ''')   
-def record_offense():
+def name_verify(driver):
     valid = False
     while valid == False:
-        driver = input("Diver Full Name: ").strip()
         names = [name for name in driver.split() if name]
         if driver == "":
             print("Do not leave blank")
@@ -21,8 +21,34 @@ def record_offense():
             print("Two names please")
         else:
             valid = True
+def licence_plate_verify(plate):
+    valid = False
+    while valid == False:
+        letter = 0
+        digits = 0   
+        letters = []
+        for char in plate:
+            if char.isdigit() == True:
+                digits += 1
+            elif char.isalpha() == True:
+                letter += 1
+            letters.append(char)
+        if letters[0].isalpha() == True and letters[1].isalpha() == True: 
+            if digits == 6 and letter == 2:
+                valid = True
+            else:
+                print('Please have 6 numbers and 2 letters')      
+        else:
+            print('Please begin plate with two letters') 
+def fine_calc():
     
-
+def warrant_check(name, outstanding):
+    if name in outstanding:
+        print('This person has an outstanding Warrant Be warned')
+def recorded_offenses():
+    driver = name_verify(input('Name of Driver: '))
+    warrant_check(driver, outstanding)
+    
 
 
 print(recorded_offenses)
